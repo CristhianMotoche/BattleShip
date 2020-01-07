c08d580d3ae04848cb4d30e309085e48d8a763dd
CristhianMotoche <cristhian.motoche@gmail.com>
2020-01-06 19:51:49 -0500

    First part to list sessions

--- client/src/BattleField/Session.elm
+++ client/src/BattleField/Session.elm
@@ -1,4 +1,40 @@
-module BattleField.Session exposing (init)
+module BattleField.Session exposing (Model, Msg, init, view)
 
-init : ()
-init = ()
+
+import Html as H
+import Html.Attributes as HA
+
+
+type alias Session =
+  { id : Int
+  , name : String
+  }
+
+
+type alias Model =
+  { sessions : List Session }
+
+
+type Msg =
+  Loading
+  | Loaded (List Session)
+
+
+init : Model
+init = {
+    sessions = []
+  }
+
+
+view : Model -> H.Html Msg
+view model =
+  H.div [ HA.class "sessions" ]
+        [ H.div []
+                (List.map viewSession model.sessions)]
+
+viewSession : Session -> H.Html Msg
+viewSession session =
+  H.div [ HA.class "session" ]
+        [ H.text (String.fromInt session.id)
+        , H.text session.name
+        ]
