a49b3efa4e138e51c6d04ca0afb8397fbc3800e8
CristhianMotoche <cristhian.motoche@gmail.com>
2020-04-13 20:35:48 -0500

    Display boards

--- client/src/BattleField/Game.elm
+++ client/src/BattleField/Game.elm
@@ -19,6 +19,9 @@ type alias Model =
 size : Int
 size = 10
 
+type alias Board = List Square
+type alias Square = (String, Int)
+
 subs : Model -> Sub Msg
 subs _ = WS.wsIn WSIn
 
@@ -49,20 +52,36 @@ view model =
   H.div
     []
     [ H.text model.msg
-    , boardView
-    , boardView
+    , boardView board
+    , boardView board
     , H.button [ HE.onClick (WSOut "Hello!") ]
                [ H.text "Send hello!" ]
     ]
 
 
-boardView : H.Html msg
-boardView =
-  H.div
-    [ HA.class "board" ]
-    <| List.repeat (size * size) squareView
+board : Board
+board =
+  let
+      alphas = String.split "" "ABCDEFGHIJ"
+      nums = List.range 1 size
+  in
+     List.concatMap (\x -> List.map (\y -> (x, y)) nums) alphas
+
+alphaIndexView : List (H.Html msg)
+alphaIndexView =
+  let
+     singleAlphaView letter = H.div [][ H.text letter ]
+  in
+     List.map singleAlphaView <| String.split "" "ABCDEFGHIJ"
 
 
-squareView : H.Html msg
-squareView =
-  H.div [ HA.class "square" ] []
+boardView : Board -> H.Html msg
+boardView b =
+  H.div [ HA.class "board" ]
+        <| List.map squareView b
+
+
+squareView : Square -> H.Html msg
+squareView (c, i) =
+  H.div [ HA.class "square" ]
+        [ H.text <| c ++ String.fromInt i]
