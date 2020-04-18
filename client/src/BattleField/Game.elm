86a8269c916b939d575b3a888d97f9ff6a1bb429
CristhianMotoche <cristhian.motoche@gmail.com>
2020-04-17 22:01:50 -0500

    Add boat position to model

--- client/src/BattleField/Game.elm
+++ client/src/BattleField/Game.elm
@@ -14,13 +14,19 @@ type alias Model =
   { key : Nav.Key
   , title : Maybe String
   , msg : String
+  , boatFirstPos : Maybe Square
+  , boatLastPos : Maybe Square
   }
 
 size : Int
 size = 10
 
 type alias Board = List (List Square)
-type alias Square = (String, Int)
+type alias Pos = (String, Int)
+type alias Square =
+  { pos : Pos
+  , usedByBoat : Bool
+  }
 
 subs : Model -> Sub Msg
 subs _ = WS.wsIn WSIn
@@ -34,6 +40,8 @@ init key sessionId =
   ({ key = key
    , title = Just "Game"
    , msg = "Waiting..."
+  , boatFirstPos = Nothing
+  , boatLastPos = Nothing
    }
   , WS.wsConnect (BR.wsURL <| BR.Session sessionId)
   )
@@ -52,19 +60,26 @@ view model =
   H.div
     []
     [ H.text model.msg
-    , boardView board
-    , boardView board
+    , H.div [ HA.class "us" ]
+            [ boardView (model.boatFirstPos, model.boatLastPos) board ]
+    , H.div [ HA.class "them" ][ boardView (Nothing, Nothing) board ]
     , H.button [ HE.onClick (WSOut "Hello!") ]
                [ H.text "Send hello!" ]
     ]
-
 
 
 alphas = String.split "" "ABCDEFGHIJ"
 nums = List.range 1 size
 
 board : Board
-board = List.map (\x -> List.map (\y -> (x, y)) nums) alphas
+board =
+  let
+      toSquare pos =
+        { pos = pos
+        , usedByBoat = False
+        }
+  in
+    List.map (\x -> List.map (\y -> toSquare (x, y)) nums) alphas
 
 alphaIndexView : List (H.Html msg)
 alphaIndexView =
@@ -81,11 +96,13 @@ numsIndexView =
      List.map singleAlphaView <| List.range 1 size
 
 
-boardView : Board -> H.Html msg
-boardView b =
+boardView : (Maybe Square, Maybe Square) -> Board -> H.Html msg
+boardView boatPosition b =
   H.div [ HA.class "board" ]
-        <| List.append (letterView "X" :: List.map numView nums)
-        <| List.concatMap alphaSquareView (List.map2 Tuple.pair alphas b)
+    <| List.append (letterView "X" :: List.map numView nums)
+    <| case boatPosition of
+        (Just fistPos, _) -> []
+        (_, _) -> List.concatMap alphaSquareView (List.map2 Tuple.pair alphas b)
 
 letterView : String -> H.Html msg
 letterView letter = H.div [ HA.class "letter" ][ H.text letter ]
@@ -98,6 +115,13 @@ alphaSquareView (letter, squares) =
   letterView letter :: List.map squareView squares
 
 squareView : Square -> H.Html msg
-squareView (c, i) =
-  H.div [ HA.class "square" ]
-        [ H.text <| c ++ String.fromInt i]
+squareView {pos, usedByBoat} =
+  let
+      (c, i) = pos
+  in
+    H.div [ HA.class "square",
+            if usedByBoat
+            then HA.class "used-by-boat"
+            else HA.class ""
+          ]
+          [ H.text <| c ++ String.fromInt i ]
---
+++ client/tests/BattleField/GameTest.elm
@@ -0,0 +1,6 @@
+module BattleField.GameTest exposing (..)
+
+import BattleField.Game as BG
+
+
+foo = ()
