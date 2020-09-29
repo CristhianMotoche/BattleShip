9bc22d279100671cd6b99f5a9a06cb2a2dab1ed4
CristhianMotoche <cristhian.motoche@gmail.com>
2020-09-28 21:38:25 -0500

    Set turn [ci skip]

--- server/tests/game/domain/use_cases/test_play.py
+++ server/tests/game/domain/use_cases/test_play.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from unittest.mock import create_autospec
 
 from battlefield.game.domain.use_cases.play import Play
-from battlefield.game.domain.entities import Game, Player, PlayerAction
+from battlefield.game.domain.entities import Game, Player, PlayerAction, PlayerPhase
 from battlefield.game.domain.use_cases.responder import ResponderClient
 
 
@@ -30,3 +30,46 @@ class PlayUnitTest(TestCase):
         Play(game, player, PlayerAction.PLACING, mock_responder).perform()
 
         mock_responder.send_to.assert_called_once_with(player, "Too many players")
+
+    def test_perform_after_successfull_attack_changes_turn(
+        self,
+    ) -> None:
+        mock_responder = create_autospec(ResponderClient, spec_set=True)
+        player1 = Player(1, 1, None, PlayerPhase.READY)
+        player2 = Player(2, 1, None, PlayerPhase.READY)
+        game = Game(1)
+        game.append(player1)
+        game.append(player2)
+
+        Play(game, player1, PlayerAction.ATTACKING, mock_responder).perform()
+
+        assert game._turn == 1
+
+    def test_perform_after_successfull_attack_from_second_player_changes_turn(
+        self,
+    ) -> None:
+        mock_responder = create_autospec(ResponderClient, spec_set=True)
+        player1 = Player(1, 1, None, PlayerPhase.READY)
+        player2 = Player(2, 1, None, PlayerPhase.READY)
+        game = Game(1)
+        game.append(player1)
+        game.append(player2)
+
+        Play(game, player1, PlayerAction.ATTACKING, mock_responder).perform()
+        Play(game, player2, PlayerAction.ATTACKING, mock_responder).perform()
+
+        assert game._turn == 0
+
+    def test_perform_when_trying_to_attack_in_another_turn_sends_error_to_player(
+        self,
+    ) -> None:
+        mock_responder = create_autospec(ResponderClient, spec_set=True)
+        player1 = Player(1, 1, None, PlayerPhase.READY)
+        player2 = Player(2, 1, None, PlayerPhase.READY)
+        game = Game(1)
+        game.append(player1)
+        game.append(player2)
+
+        Play(game, player2, PlayerAction.ATTACKING, mock_responder).perform()
+
+        mock_responder.send_to.assert_called_once_with(player2, "Not your turn")
